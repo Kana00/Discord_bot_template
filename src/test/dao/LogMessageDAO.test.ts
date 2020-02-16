da393e7c7ca88ccb8f0c860f96761b2fe1a4bd88
illya Delsarte <illyadelsarte@MacBook-Pro-de-illya.local>
2020-02-16 18:05:06 +0100

    Add Jest for unit testing

---
+++ src/test/dao/LogMessageDAO.test.ts
@@ -0,0 +1,10 @@
+import LogMessageDAO from '../../dao/LogMessageDAO';
+import Mongo from '../../utils/Mongo';
+
+describe('Test class LogMessageDAO', () => {
+
+  it('LogMessageDAO-saveToDB', () => {
+    expect(1+2).toBe(3);
+  });
+
+});
